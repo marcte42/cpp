e7b2bb30f714092959ac7339363bb4ccc8667ec3
marcte42 <marcte42@gmail.com>
2022-05-11 14:37:50 +0200

    main tweaks + Makefile

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -8,22 +8,44 @@
 
 int main()
 {
-	IMateriaSource *src = new MateriaSource();
+	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	ICharacter *me = new Character("me");
-	AMateria *tmp;
-	tmp = src->createMateria("ice");
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	ICharacter* bob = new Character("bob");
+
+	AMateria* ice = src->createMateria("ice");
+	me->equip(ice);
+	AMateria* tmp;
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("fire");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	ICharacter *bob = new Character("bob");
+
 	me->use(0, *bob);
-	me->unequip(1);
 	me->use(1, *bob);
-	delete tmp;
+	me->use(2, *bob);
+	me->use(3, *bob);
+	me->use(100, *bob);
+
+	std::cout << std::endl;
+
+	me->unequip(0);
+	delete ice;
+	me->use(0, *bob);
+	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+	me->unequip(100);
+	me->use(100, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
+
 	return 0;
 }
