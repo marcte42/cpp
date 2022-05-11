65e0f5dd07eaf02858bb5509095ac85458451655
marcte42 <marcte42@gmail.com>
2022-05-11 14:22:47 +0200

    main tweaks smaller tab

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,11 +6,11 @@ int main()
 {
 	Animal *tabAnimal[100];
 
-	for (int i = 0; i < 50; i++)
+	for (int i = 0; i < 5; i++)
 		tabAnimal[i] = new Dog();
-	for (int i = 50; i < 100; i++)
+	for (int i = 5; i < 10; i++)
 		tabAnimal[i] = new Cat();
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10; i++)
 		delete tabAnimal[i];
 
 	{
