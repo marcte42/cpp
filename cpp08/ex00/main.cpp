84a67a6d466b881d76a87b99074020f104089ee2
marcte42 <marcte42@gmail.com>
2022-05-25 16:52:12 +0200

    main with char test

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,27 +4,51 @@
 
 int main()
 {
-	std::vector<int> v;
-
-	v.push_back(0);
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-
-	try
 	{
-		for (int i = 0; i < 10; i++)
+		std::vector<int> v;
+
+		v.push_back(0);
+		v.push_back(1);
+		v.push_back(2);
+		v.push_back(3);
+		v.push_back(4);
+		v.push_back(5);
+
+		try
 		{
-			easyfind(v, i);
-			std::cout << i << " found" << std::endl;
+			for (int i = 0; i < 10; i++)
+			{
+				easyfind(v, i);
+				std::cout << i << " found" << std::endl;
+			}
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
 		}
 	}
-	catch (std::exception &e)
 	{
-		std::cout << "Not found" << std::endl;
-	}
+		std::vector<char> v;
 
+		v.push_back('a');
+		v.push_back('b');
+		v.push_back('c');
+		v.push_back('d');
+		v.push_back('e');
+		v.push_back('f');
+
+		try
+		{
+			for (char i = 'a'; i < 'z'; i++)
+			{
+				easyfind(v, i);
+				std::cout << i << " found" << std::endl;
+			}
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
 	return 0;
 }
